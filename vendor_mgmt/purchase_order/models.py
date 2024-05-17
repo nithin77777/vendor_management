f9c55b0145e8716e21448c78080b7089163368a1
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2024-05-17 06:39:17 -0400

    application - purchase order

---
+++ vendor_mgmt/purchase_order/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from vendor_profile.models import VendorProfile
+
+# Create your models here.
+STATUS_CHOICES = [('pending','PENDING'),('completed','COMPLETED'),('canceled','CANCELED')]
+
+class PurchaseOrder(models.Model): 
+
+    po_number=models.CharField(verbose_name='Purchase Order Number',max_length=20,primary_key=True) 
+    vendor=models.ForeignKey(VendorProfile,on_delete=models.CASCADE)
+    order_date=models.DateTimeField()
+    delivery_date=models.DateTimeField()
+    items=models.JSONField()
+    quantity=models.IntegerField()
+    status=models.CharField(choices=STATUS_CHOICES,max_length=20)
+    quality_rating=models.FloatField(null=True)
+    issue_date=models.DateTimeField()
+    acknowledgment_date=models.DateTimeField(null=True)
