f7e27f8568f732f3ccc1a195495f27919735cbfb
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2024-05-18 16:25:20 -0400

    Made Templates for Purchase Orders , updated urls Created Views 	new file:   purchase_order/templates/all_orders.html 	new file:   purchase_order/templates/purchase_orders.html 	modified:   purchase_order/urls.py 	modified:   purchase_order/views.py

--- vendor_mgmt/purchase_order/urls.py
+++ vendor_mgmt/purchase_order/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 
+from .views import create_order,all_orders
+
 urlpatterns = [
     
-    # path(),
+    path("",view=create_order),
+    path('orders/',view=all_orders),
 ]
--- vendor_mgmt/purchase_order/views.py
+++ vendor_mgmt/purchase_order/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import PurchaseOrder
+from vendor_profile.models import VendorProfile
 
 # Create your views here.
+
+
+
+
+def create_order(request):
+    
+    if request.method =='POST':
+        po = request.POST.get('po_number')
+        vendor = request.POST.get('vendor')
+        order_date=request.POST.get('order_date')
+        delivery_date=request.POST.get('delivery_date')
+        items=request.POST.get('items')
+        quantity=request.POST.get('quantity')
+        status=request.POST.get('status')
+        quality_rating=request.POST.get('quality_rating')
+        issue_date = request.POST.get('issue_date')
+        acknowledgment_date=request.POST.get('acknowledgment_date')
+
+        vendor = VendorProfile.objects.get(name=vendor)
+
+        data = PurchaseOrder(
+            po_number=po,
+            vendor=vendor,
+            order_date=order_date,
+            delivery_date=delivery_date,
+            items=items,
+            quantity=quantity,
+            quality_rating=quality_rating,
+            status=status,
+            issue_date=issue_date,
+            acknowledgment_date=acknowledgment_date
+        )
+        
+        data.save()
+        return redirect("orders/")
+    return render(request,'purchase_orders.html')
+
+def all_orders(request):
+    orders = PurchaseOrder.objects.all()
+    return render(request,"all_orders.html",{'orders':orders})
