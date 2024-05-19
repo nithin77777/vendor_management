7cc7403fd4575e9b83e538b09cd66d6453b54a66
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2024-05-18 22:30:33 -0400

    Update View Added Updating through vendor id --> When vendor id is mentioned in url correctly.

--- vendor_mgmt/vendor_profile/urls.py
+++ vendor_mgmt/vendor_profile/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('vendors/',view=all_data),
     path('',view=create_vendor),
     path('<str:v_id>/',view=specific_vendor),
+    path('put/<str:vendor_id>/',view=put_vendor,name="put_vendor"),
+    # path(),
 
 ]
--- vendor_mgmt/vendor_profile/views.py
+++ vendor_mgmt/vendor_profile/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import VendorProfile
 
 # Create your views here.
@@ -40,4 +44,26 @@ def specific_vendor(request,v_id):
     vendor_code = str(v_id).upper()
 
     data = VendorProfile.objects.filter(vendor_code=vendor_code)
+    
     return render(request,'vendor_specific.html',context={'data':data})
+
+
+
+def put_vendor(request,vendor_id):
+
+    v_code = str(vendor_id).upper()
+    
+    data  = VendorProfile.objects.filter(vendor_code=v_code).first()
+    # print(data.vendor_code)
+    
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        contact_details = request.POST.get("contact_details")
+        address=request.POST.get('address')
+        vendor_code = data.vendor_code#request.POST.get('vendor_code')
+        data = VendorProfile(name=name, contact_details=contact_details, address=address)
+        data.save()
+        messages.success(request,"Updated {vendor_id} Successfully")
+        return redirect(reverse("put_vendor",kwargs={"vendor_id":vendor_id}))
+    messages.success(request,f"Updated {vendor_id} Successfully")
+    return render(request,"update_vendor.html",context={"updated_data":data})
